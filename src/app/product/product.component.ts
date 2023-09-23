de00c64bc409de86b29c47d6552164aaa3ed864d
Naderab <abdellaouinader@gmail.com>
2023-09-23 16:13:55 +0100

    manipulation des composants séance 2

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/core/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent {
+  title: string = 'Products List';
+  color:string=''
+  product: Product = {
+    id: '1',
+    name: 'phone',
+    price: 2000,
+    quantity: 5,
+  };
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'phone',
+      price: 2000,
+      quantity: 5,
+    },
+    {
+      id: '2',
+      name: 'tv',
+      price: 1500,
+      quantity: 0,
+    },
+    {
+      id: '3',
+      name: 'PC',
+      price: 3000,
+      quantity: 3,
+    },
+  ];
+
+  getState(p: Product) {
+    console.log(p);
+    if (p.quantity != 0) {
+      console.log('true');
+      return true;
+    } else {
+      console.log('false');
+      return false;
+    }
+  }
+
+  Acheter(p: Product) {
+    p.quantity--;
+  }
+
+  getColor() {
+    return this.color;
+  }
+}
