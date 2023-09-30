c4a24a3dca57af92b396204427282a457ac548b5
Naderab <abdellaouinader@gmail.com>
2023-09-30 15:58:17 +0100

    correction atelier 2 "manipulation des composants"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
+import { FournisseurComponent } from './fournisseur/fournisseur.component';
+import { ListFournisseurComponent } from './list-fournisseur/list-fournisseur.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    FournisseurComponent,
+    ListFournisseurComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/list-fournisseur/list-fournisseur.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Fournisseur } from 'src/core/fournisseur';
+
+@Component({
+  selector: 'app-list-fournisseur',
+  templateUrl: './list-fournisseur.component.html',
+  styleUrls: ['./list-fournisseur.component.css'],
+})
+export class ListFournisseurComponent {
+  fournisseurs: Fournisseur[] = [
+    { idFournisseur: 1, code: '1ABC4522', libelle: 'hp' },
+    { idFournisseur: 2, code: '2ABC4522', libelle: 'dell' },
+    { idFournisseur: 3, code: '3ABC4522', libelle: 'lenovo' },
+    { idFournisseur: 400, code: '40ABC452', libelle: 'asus' },
+  ];
+
+  deleteFournisseur(f: Fournisseur) {
+    const index = this.fournisseurs.indexOf(f);
+    if (index != -1) {
+      this.fournisseurs.splice(index, 1);
+    }
+  }
+}
