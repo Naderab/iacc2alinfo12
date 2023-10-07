3b4eabd3eaf25ef2986246d138d4959554ba6f99
Naderab <abdellaouinader@gmail.com>
2023-10-07 09:13:19 +0100

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ListFournisseurComponent } from './list-fournisseur/list-fournisseur.component';
+import { FournisseurComponent } from './fournisseur/fournisseur.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'products', component: ProductComponent },
+  { path: 'fournisseurs', component: ListFournisseurComponent },
+  { path: 'fournisseur', component: FournisseurComponent },
+  { path: '',redirectTo:'products',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
